af44eb8d3b426011bc8087b62645c9799ef785ba
Santos <santosphone22@gmail.com>
2025-04-18 22:34:24 -0600

    .env

--- backend-to-do-list/src/usuarios/module.service.ts
+++ backend-to-do-list/src/usuarios/module.service.ts
@@ -1,24 +1,28 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { Usuario } from './entities/usuario.entity';  // Ajusta la ruta según tu archivo de entidad
+import { Usuario } from './entities/usuario.entity';  // Adjust the path to your entity
 import { Role } from 'src/common/enums/rol.enum';
+import { ConfigService } from '@nestjs/config';  // You might need to inject ConfigService for config
 
 @Injectable()
 export class UserService implements OnModuleInit {
     constructor(
         @InjectRepository(Usuario)
         private readonly usuarioRepository: Repository<Usuario>,
+        private readonly configService: ConfigService,
     ) { }
 
     async onModuleInit() {
-        const userExists = await this.usuarioRepository.findOne({ where: { email: 'reto@blindariesgos.com' } });
+        const email = this.configService.get<string>('COREO');
+
+        const userExists = await this.usuarioRepository.findOne({ where: { email } });
 
         if (!userExists) {
             await this.usuarioRepository.save({
                 role: Role.USER,
                 name: 'Blindariesgos',
-                email: 'reto@blindariesgos.com',
+                email: email,
                 password: 'Reto123',
             });
             console.log('Usuario de prueba insertado.');
